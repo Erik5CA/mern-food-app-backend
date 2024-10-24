a562b70fdc0fd41e6a8288a205aac3a7081f118f
Erik5CA <erik.castillo.anzures@gmail.com>
2024-10-23 22:27:58 -0600

    first commit

---
+++ src/routes/myUserRoutes.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import myUserControllers from "../controllers/myUserControllers";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validations";
+
+const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, myUserControllers.getCurrentUser);
+
+router.post("/", jwtCheck, myUserControllers.createCurrentUser);
+
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  myUserControllers.updateCurrentUser
+);
+
+export default router;
