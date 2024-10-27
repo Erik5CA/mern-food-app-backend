07d824c4096ccfaa5f7e09158408f27805c463db
Erik5CA <erik.castillo.anzures@gmail.com>
2024-10-27 13:39:06 -0600

    adding user profile feature

--- src/middleware/validations.ts
+++ src/middleware/validations.ts
@@ -26,3 +26,27 @@ export const validateMyUserRequest = [
     .withMessage("Country must be a string."),
   handleValidationErrors,
 ];
+
+export const validateMyRestauratRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City is required"),
+  body("country").notEmpty().withMessage("Country is required"),
+  body("deliveryPrice")
+    .isFloat({ min: 0 })
+    .withMessage("Delivery price must be a positive number"),
+  body("estimateDeliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimate Delivery Time price must be a positive number"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be an array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisines array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be an array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu Item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu Item price must be a positive number"),
+  handleValidationErrors,
+];
---
+++ src/routes/myRestaurantRoute.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import multer from "multer";
+import myRestaurantController from "../controllers/myRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestauratRequest } from "../middleware/validations";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5 mb
+  },
+});
+
+router.get("/", jwtCheck, jwtParse, myRestaurantController.getMyRestaurant);
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestauratRequest,
+  jwtCheck,
+  jwtParse,
+  myRestaurantController.createMyRestaurant
+);
+
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestauratRequest,
+  jwtCheck,
+  jwtParse,
+  myRestaurantController.updateMyRestaurant
+);
+
+export default router;
